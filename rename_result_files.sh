83334319cd0b8e4aa11a54b5d2bb3cdeb41121ab
FJShen <shen449@purdue.edu>
2021-09-09 15:38:23 -0400

    changed mv to cp in rename script

--- rename_result_files.sh
+++ rename_result_files.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-DIR_NAME="2021_8_19"
+DIR_NAME="2021_9_5"
 
 for file in extracted_reports/${DIR_NAME}/q*/result.csv/*.csv; do 
     echo $file
-    query_name="q"$(echo "$file" | sed -E 's|extracted_reports/2021_8_19/q([0-9]+).*|\1|')
-    result_path=$(echo "$file" | sed -E 's|(extracted_reports/2021_8_19/q[0-9]+/result.csv/).*|\1|')
+    query_name="q"$(echo "$file" | sed -E 's|extracted_reports/2021_9_5/q([0-9]+).*|\1|')
+    result_path=$(echo "$file" | sed -E 's|(extracted_reports/2021_9_5/q[0-9]+/result.csv/).*|\1|')
     echo $query_name
     echo $result_path
-    mv $file ${result_path}${query_name}.csv 
+    cp $file ${result_path}${query_name}.csv 
 done
